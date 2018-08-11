cf458cd6b215d2d33159f1dd0c080c7beb0b2b99
vagrant <vagrant@homestead>
2018-08-11 03:41:48 +0000

    1.0.1

--- src/DumpDie.php
+++ src/DumpDie.php
@@ -3,9 +3,14 @@
 namespace MazeCode;
 
 use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
 
 class DumpDie
 {
+    public function __construct()
+    {
+        // parent::__construct();
+    }
     /**
      * Dump a value with elegance.
      *
@@ -15,7 +20,9 @@ class DumpDie
     public static function dd($param)
     {
         array_map(function ($arg) {
-            $dd = new HtmlDumpDie;
+            $arg = is_object($arg) ? new \ReflectionClass($arg) : $arg;
+			$dd = PHP_SAPI === 'cli' ? new CliDumper() : new HtmlDumpDie(null, 'utf-8');
+			var_dump($dd);
             $dd->dump((new VarCloner)->cloneVar($arg));
         }, $param);
         die;
--- src/HtmlDumpDie.php
+++ src/HtmlDumpDie.php
@@ -6,24 +6,42 @@ use Symfony\Component\VarDumper\Dumper\HtmlDumper;
 
 class HtmlDumpDie extends HtmlDumper
 {
-    /**
-     * Colour definitions for output.
-     *
-     * @var array
-     */
-    protected $styles = [
-        'default' => 'background-color:#fff !important; color:#222; line-height:1.2em; font-weight:normal; font:12px Monaco, Consolas, monospace; word-wrap: break-word; white-space: pre-wrap; position:relative; z-index:100000',
-        'num' => 'color:#a71d5d',
-        'const' => 'color:#795da3',
-        'str' => 'color:#df5000',
-        'cchr' => 'color:#222',
-        'note' => 'color:#a71d5d',
-        'ref' => 'color:#a0a0a0',
-        'public' => 'color:#795da3',
-        'protected' => 'color:#795da3',
-        'private' => 'color:#795da3',
-        'meta' => 'color:#b729d9',
-        'key' => 'color:#df5000',
-        'index' => 'color:#a71d5d',
-    ];
+    public static $defaultOutput = 'php://output';
+    protected $dumpHeader;
+    protected $dumpPrefix     = '<pre class=sf-dump id=%s data-indent-pad="%s">';
+    protected $dumpSuffix     = '</pre><script>Sfdump(%s)</script>';
+    protected $dumpId         = 'sf-dump';
+    protected $colors         = true;
+    protected $headerIsDumped = true;
+    protected $lastDepth      = -1;
+    protected $styles         = array(
+		'default'   => 'background-color:#ffffff; color:#FF8400; line-height:1.2em; font:12px "Fira Code", "Monoid", monospace; word-wrap: break-word; white-space: pre-wrap; position:relative; z-index:99999; word-break: break-all',
+        'num'       => 'font-weight:bold; color:#1299DA; font:12px "Fira Code", "Monoid", monospace; ',
+        'const'     => 'font-weight:bold; font:12px "Fira Code", "Monoid", monospace; ',
+        'str'       => 'font-weight:bold; color:#56DB3A; font:12px "Fira Code", "Monoid", monospace; ',
+        'note'      => 'color:#1299DA; font:12px "Fira Code", "Monoid", monospace;',
+        'ref'       => 'color:#A0A0A0; font:12px "Fira Code", "Monoid", monospace;',
+        'public'    => 'color:#000000; font:12px "Fira Code", "Monoid", monospace;',
+        'protected' => 'color:#000000; font:12px "Fira Code", "Monoid", monospace;',
+        'private'   => 'color:#000000; font:12px "Fira Code", "Monoid", monospace;',
+        'meta'      => 'color:#B729D9; font:12px "Fira Code", "Monoid", monospace;',
+        'key'       => 'color:#56DB3A; font:12px "Fira Code", "Monoid", monospace;',
+        'index'     => 'color:#1299DA; font:12px "Fira Code", "Monoid", monospace;',
+        'ellipsis'  => 'color:#FF8400; font:12px "Fira Code", "Monoid", monospace;',
+        'ns'        => 'user-select:none;',
+    );
+    private $displayOptions = array(
+        'maxDepth'        => 3,
+        'maxStringLength' => 160,
+        'fileLinkFormat'  => null,
+    );
+    private $extraDisplayOptions = array();
+
+    public function __construct()
+    {
+		parent::__construct();
+		$this->setDisplayOptions($this->displayOptions);
+    }
 }
+
+dump(new HtmlDumpDie());
